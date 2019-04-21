c81d66e17ff5282ea37ea75008e57d6e5500c9db
Patrick Doyle <patrickdoyle30@gmail.com>
2019-04-21 16:31:40 -0700

    Initial commit

--- MMACalendar/ViewController.swift
+++ MMACalendar/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import SwiftSoup
+import Dispatch
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadWebpage()
     }
+    
+    func loadWebpage() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let url = URL(string: "https://www.mmafighting.com/schedule") {
+                do {
+                    let contents = try String(contentsOf: url)
+                    let html = contents
+                    let doc: Document = try SwiftSoup.parse(html)
+                    let h3: Array<Element> = try doc.select("h3").array()
+                    let fights: Array<Element> = try doc.select("a").array()
+                    for fight in fights{
+                        let title = try fight.text()
+                        let href = try fight.attr("href")
+                        if title != nil && href != nil && href.contains("fight-card") {
+                            print("Event Name" + title)
+                        
+                        } else if title != nil && href != nil && href.contains("/fight/") {
+                            print("Fight Data " + title)
+                        }
+                    }
+                    for date in h3 {
+                        print(date)
+                    }
+                } catch {
+                    // contents could not be loaded
+                }
+            } else {
+                // the URL was bad!
+            }
+        }
+        
+    }
+  
 
 
 }
+
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '9.0'
+
+target 'MMACalendar' do
+  use_frameworks!
+
+pod 'SwiftSoup', '1.7.4'
+
+end
